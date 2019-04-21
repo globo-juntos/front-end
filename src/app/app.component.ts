6065fa5aa9da90312fb8d0b2e4535b19094d42e8
AugustoBondanca <augustobondancaa@gmail.com>
2019-04-21 08:52:53 -0300

    cont and options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,31 +8,53 @@ import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 })
 export class AppComponent {
 
-  ngOnInit() {
-
-    setTimeout(() => {
-      this.showSnack()
-      this.showSnack2()
-    }, 8000);
+  constructor(private http: HttpClient) {
 
   }
 
   @ViewChild('video')
 
   public video: ElementRef;
-
   currentTime: any;
   nexVideo = 29.910418;
   validate = false;
   vingadores = "../assets/video/Vingadores - Ultimato - É o fim.mp4"
+  url = "https://globojuntos.firebaseio.com"
+  totalVotos: number;
+  primeiraOpcao = 'esta é a opção 1'
+  segundaOpcao = 'esta é a opção 2'
+  primeiroOpcaoVoto: number;
+  segundoOpcaoVoto: number;
+
+
+  ngOnInit() {
+
+    setTimeout(() => {
+      this.showSnack()
+      this.showSnack2()
+    }, 8000);
+    this.getVote()
+  }
+  getVote() {
+    this.http.get(this.url + '/votos.json').subscribe((data: any) => {
+      7
+      this.totalVotos = data.op1 + data.op2
+      this.primeiroOpcaoVoto = data.op1;
+      this.segundoOpcaoVoto = data.op2;
+      console.log('data', data)
+    })
+
+  }
 
   setCurrentTime(data) {
     this.currentTime = data.target.currentTime;
     if (this.currentTime > this.nexVideo) {
-      this.validate = true;
-      this.video.nativeElement.src = "../assets/video/Vingadores - Ultimato - É o fim.mp4";
-      this.video.nativeElement.load();
-      this.video.nativeElement.play();
+      if (this.primeiroOpcaoVoto > this.segundoOpcaoVoto) {
+        this.video.nativeElement.src = "../assets/video/Vingadores - Ultimato - É o fim.mp4";
+      }
+      else if (this.primeiroOpcaoVoto < this.segundoOpcaoVoto) {
+        this.video.nativeElement.src = "../assets/video/Vingadores - Ultimato - É o fim.mp4";
+      }
 
     }
   }
@@ -45,8 +68,5 @@ export class AppComponent {
     var x = document.getElementById("snackbar2");
     x.className = "show";
     setTimeout(function () { x.className = x.className.replace("show", ""); }, 22000);
-  }
-  showSnackAll() {
-
   }
 }
